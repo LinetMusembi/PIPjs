39b9b9793cad5d6e8a79ec0b73becc0313e51e1c
linetmusembi <musembilinetmwongeli@gmail.com>
2023-05-06 20:46:13 +0300

    first commit

--- arrays.js
+++ arrays.js
@@ -1,49 +1,53 @@
 // Given an array of strings, use a function to reverse all the elements in the string in ascending order 
 // and the specific elements in descending order
 
-function sortArr(arr){
+// function sortArr(arr){
+//     return arr.sort().reverse("").split("")
 
-}
+// }
+// console.log(sortArr(arr["learning"]))
 
 // Given an array of objects, each object representing a person with a name and age property, 
 // write a function that returns the sum of all people who are less than 18 years.
 
 
-const people = [
+const num = [
   { name: 'Alice', age: 17 },
   { name: 'Eunice', age: 22 },
   { name: 'Charlie', age: 14 },
   { name: 'Max', age: 19 },
 ];
 
-function sumAge(age) {
+function sumAge(num) {
     let sum = 0;
-    for (let i = 0; i < age.length; i++) {
-      if (age[i].age < 18) {
-        sum += age[i].age;
+    for (let i = 0; i < num.length; i++) {
+      if (num[i].age < 18) {
+        sum += num[i].age;
       }
     }
     return sum;
   }
-  console.log(sumAge(age));
+  console.log(sumAge(num));
 
 // Using JS functions and an array of numbers, return positive if an element within the array is positive,
 //  negative if an element is negative, else zero
-function checkNum(nums) {
-    let positive = 0;
-    for (let i = 0; i < arr.length; i++) {
-      if (nums[i] > 0) {
-        positive = "positive";
-        break;
-      } else if (nums[i] < 0) {
-        positive = "negative";
-        break;
-      }
-    }
-    return positive;
-  }
-  let nums= [1, 2, 3, 4, 5];
-  console.log(checkNum(nums));
+
+function checkNum(arr) {
+   if(arr>=0){
+    return "positive"
+   }
+   else if(arr<0){
+    return "negative"
+   }
+   else{
+    return "zero"
+   }
+
+
+}
+arr = [-7]
+console.log(checkNum(arr))
+
 
 // Given an array of objects, where each object represents an employee with an id, name, and salary property,
 //  write a function that returns a new array of employee objects sorted by their salary in ascending order.
